65a839402f0cbd89644847e7c45d06651e41dd95
rana.lubis <rana.lubis@gmail.com>
2022-03-16 14:27:00 +0700

    update version

--- src/actions/createHttpServer.ts
+++ src/actions/createHttpServer.ts
@@ -1,8 +1,74 @@
 import http from "http";
+import httpProxy from "http-proxy";
+
 import { logger } from "../logger";
 
+const getSocketPath = (
+  urlPaths: string[],
+  client_req: http.IncomingMessage
+) => {
+  const sortedPaths = Object.keys(urlPaths)
+    .sort((a, b) => b.split("/").length - a.split("/").length)
+    .map((key) => ({ basePath: key, socketPath: urlPaths[key] }));
+  // const urlTokens = client_req.url.split("/").filter((p) => p !== "");
+
+  let socketPath = null;
+  for (const p of sortedPaths) {
+    if (client_req.url.startsWith(p.basePath)) {
+      socketPath = p.socketPath;
+
+      break;
+    }
+  }
+
+  return socketPath;
+};
+
 export default function createHttpServer(urlPaths: string[]) {
+  const proxy = httpProxy.createProxyServer({ ws: true });
+
+  proxy.on("error", (err) => console.log(err));
   const server = http.createServer((client_req, client_res) => {
+    const socketPath = getSocketPath(urlPaths, client_req);
+    if (socketPath) {
+      proxy.web(
+        client_req,
+        client_res,
+        {
+          target: { socketPath: socketPath } as any,
+        },
+        (err) => {
+          logger.error(err);
+
+          client_res.statusCode = 500;
+          client_res.end("Server not ready");
+        }
+      );
+    } else {
+      client_res.statusCode = 404;
+      client_res.end("Page Not Found");
+    }
+  });
+
+  server.on("upgrade", (client_req, socket, head) => {
+    const socketPath = getSocketPath(urlPaths, client_req);
+    // console.log(client_req.url, socketPath);
+    if (socketPath)
+      proxy.ws(
+        client_req,
+        socket,
+        head,
+        { target: { socketPath: socketPath } as any },
+        (err) => {
+          logger.error(err);
+
+          socket.end();
+        }
+      );
+    else socket.end();
+  });
+
+  /* const server = http.createServer((client_req, client_res) => {
     const sortedPaths = Object.keys(urlPaths)
       .sort((a, b) => b.split("/").length - a.split("/").length)
       .map((key) => ({ basePath: key, socketPath: urlPaths[key] }));
@@ -34,6 +100,15 @@ export default function createHttpServer(urlPaths: string[]) {
           }
         );
 
+        proxy.on("error", (err) => {
+          logger.error(err);
+
+          client_res.statusCode = 500;
+          client_res.end("Service is not ready");
+        });
+        // proxy.on("close", () => console.log("connection closed"));
+        // proxy.on("socket", () => console.log("socket connected"));
+
         client_req.pipe(proxy, {
           end: true,
         });
@@ -46,7 +121,7 @@ export default function createHttpServer(urlPaths: string[]) {
       client_res.statusCode = 404;
       client_res.end("Page Not Found");
     }
-  });
+  }); */
 
   return server;
 }
